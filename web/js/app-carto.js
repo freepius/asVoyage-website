7efec891b701dc7ecbf2dc7a524c9dae66d0e198
freepius <freepius@anarchos-semitas.net>
2013-09-08 20:26:29 +0200

    Clarify using of strong, em, b and i + Improve carto toolbox

--- web/js/app-carto.js
+++ web/js/app-carto.js
@@ -12,34 +12,29 @@
 
         layers: {
             OSM: new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, CC-BY-SA'
             }),
             WaterColor: new L.StamenTileLayer('watercolor'),
             BingAerial: new L.BingLayer('Aj6KakrmmAztTyUmXMu7wJnHplOuIYmGbXdd5brEpsAFk3nZL57oPmFgV47nMNHp')
         },
 
         addMap: function (id, options) {
-            var map, attr,
+            var attr, map,
 
                 // default options
                 o = {
                     center  : [0.0, 0.0],
                     zoom    : 5,
-                    minZoom : 1,
-                    maxZoom : 18,
-                    layer   : 'WaterColor'
+                    layer   : 'WaterColor',
+                    layers  : []
                 };
 
             // Merge default and user options
             for (attr in options) { o[attr] = options[attr]; }
 
-            map = L.map(id, {
-                center  : o.center,
-                zoom    : o.zoom,
-                minZoom : o.minZoom,
-                maxZoom : o.maxZoom,
-                layers  : [this.layers[o.layer]]
-            });
+            o.layers.push(this.layers[o.layer]);
+
+            map = L.map(id, o);
 
             map.attributionControl.setPrefix('');
 
